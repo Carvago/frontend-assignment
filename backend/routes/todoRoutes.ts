57e24f90e0f03cfca123d8c0e0aa39a6409ee7ad
nikita <nikita.filly@gmail.com>
2026-03-16 13:51:48 +0100

    fix: changin BE logic for no found todo

--- backend/routes/todoRoutes.ts
+++ backend/routes/todoRoutes.ts
@@ -27,7 +27,7 @@ todoRoutes.get('/api/todo/:id', authenticateToken, (req: Request, res: Response)
 
   todoDB.findOne({id: todoId, userId: data.userId}, (error: Error | null, todo: Todo) =>
     match(isNil(error))
-      .with(true, () => res.status(200).json(todo))
+      .with(true, () => isNil(todo) ? res.status(404).json({error: 'Todo not found'}) : res.status(200).json(todo))
       .otherwise(() => res.status(500).json({error: 'Internal Server Error'}))
   );
 });
